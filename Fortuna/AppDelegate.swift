9cef925036ecccd875d490d9e053a911abcf48da
Jinma li <lijinma@126.com>
2014-10-17 14:43:32 +0800

    - import all files

--- Fortuna/AppDelegate.swift
+++ Fortuna/AppDelegate.swift
@@ -13,12 +13,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var positiveQuotes: [String]!
+    var negativeQuotes: [String]!
+    
+    let positiveQuotesPath = NSBundle.mainBundle().pathForResource("positiveQuotes", ofType: "json");
+    let negativeQuotesPath = NSBundle.mainBundle().pathForResource("negativeQuotes", ofType: "json");
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // TODO: load quotations from JSON files
+
+        
+        if let path = positiveQuotesPath {
+            positiveQuotes = loadJSON(path) as [String]
+        }
+        
+        if let path = negativeQuotesPath {
+            negativeQuotes = loadJSON(path) as [String]
+        }
+        
+        
+        // Assertions to make sure that the quotations are loaded.
+        assert(positiveQuotes.count > 0, "should load positive quotes")
+        assert(negativeQuotes.count > 0, "should load negative quotes")
         return true
     }
 
+    func loadJSON(path: String) -> AnyObject? {
+        // Load data from path
+        let data = NSData(contentsOfFile: path)
+        assert(data != nil, "Failed to read data from: \(path)")
+        
+        // Parse JSON data
+        var err : NSError?
+        let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.allZeros, error: &err)
+        assert(err == nil, "Error parsing json: \(err)")
+        
+        return json
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -40,7 +74,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
--- Fortuna/ViewController.swift
+++ Fortuna/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var quotationTextView: UITextView!
 
+    @IBAction func rootViewTapped(sender: UITapGestureRecognizer) {
+        displayRandomQuote()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        displayRandomQuote()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func displayRandomQuote() {
+        var delegate = UIApplication.sharedApplication().delegate as AppDelegate
+        
+        // quotes = radomly choose positiveQuotations or negativeQuotations
+        // quote = randomly choose an element from quotes
+        var quotesSet = [delegate.positiveQuotes, delegate.negativeQuotes]
+        var quotes = quotesSet[getRandomIndex(quotesSet.count)]
+        var quote = quotes[getRandomIndex(quotes.count)];
+
+        quotationTextView.text = quote
+        quotationTextView.editable = false
+        quotationTextView.selectable = false
+    }
+    
+    func getRandomIndex (count: Int) ->Int{
+        return Int(arc4random_uniform(UInt32(count)))
+    }
 
 }
 
