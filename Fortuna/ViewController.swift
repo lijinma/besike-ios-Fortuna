4b5ac22da7777a8218c609a6e8a6d597fbb53515
Jinma li <lijinma@126.com>
2014-10-17 13:23:48 +0800

    Initial Commit

---
+++ Fortuna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fortuna
+//
+//  Created by Li JinMa on 14-10-17.
+//  Copyright (c) 2014年 Li JinMa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
